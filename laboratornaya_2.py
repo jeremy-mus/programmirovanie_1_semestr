fe5ce5f6391d60b8eb7b1ddb8c51a9dded7fa47e
jeremy-mus <145211896+jeremy-mus@users.noreply.github.com>
2024-01-21 20:44:27 +0300

    Update laboratornaya_2.py

--- laboratornaya_2.py
+++ laboratornaya_2.py
@@ -1,5 +1,8 @@
 import time
+
 def possibleMoves(x, y):
+    # Функция возвращает множество возможных ходов для фигуры на доске с текущими координатами (x, y).
+
     moves = {
         (x - 3, y),
         (x - 2, y - 1), (x - 2, y + 1),
@@ -11,65 +14,81 @@ def possibleMoves(x, y):
     }
     return moves
 
-
 def figureDislocation(x, y, matrix):
+    # Функция помечает текущую клетку (x, y) фигурой и обозначает возможные ходы фигуры на доске matrix.
+
     matrix[x][y] = '#'
     for i, j in possibleMoves(x, y):
         if 0 <= i < len(matrix) and 0 <= j < len(matrix):
             matrix[i][j] = '*'
     return matrix
 
-
 def otherFiguresDislocation(matrix, figures):
+    # Функция помещает фигуры на доску matrix.
+    
     for x, y in figures:
         figureDislocation(x, y, matrix)
     return matrix
 
-
 def boardInitializer(N):
+    # Функция инициализирует доску NxN, заполняя ее нулями.
+    
     return [['0' for _ in range(N)] for _ in range(N)]
 
-
-def print_board(matrix):
+def boardPrinter(matrix):
+    # Функция выводит текущее состояние доски в консоль.
+    
     for row in matrix:
         print(" ".join(row))
 
-
 def recursia(N, L, solutions, currentSolution, figureCount):
+    # Рекурсивная функция для нахождения расстановок фигур на доске.
+
     if figureCount == L:
+        # Проверяем, не добавлено ли уже текущее решение в множество решений
         if tuple(sorted(currentSolution)) not in solutions:
             solutions.add(tuple(sorted(currentSolution)))
-            print_board(otherFiguresDislocation(boardInitializer(N), tuple(sorted(currentSolution))))
+            # Выводим доску с текущим решением в консоль
+            boardPrinter(otherFiguresDislocation(boardInitializer(N), tuple(sorted(currentSolution))))
         return
 
     for i in range(N):
         for j in range(N):
+            # Проверяем, что текущая клетка не занята другой фигурой и нет пересечений с возможными ходами
             if all((moves not in currentSolution) for moves in possibleMoves(i, j)):
+                # Добавляем текущую клетку в решение и рекурсивно вызываем функцию для следующей фигуры
                 currentSolution.append((i, j))
                 recursia(N, L, solutions, currentSolution, figureCount + 1)
+                # Отменяем последний выбор для backtracking
                 currentSolution.pop()
 
-
 if __name__ == "__main__":
+    # Чтение входных данных из файла
     file = open("input.txt", "r")
     N, L, K = map(int, file.readline().split())
 
     figuresOnTheBoard = []
     solutions = set()
 
+    # Чтение координат фигур из файла
     for line in file.readlines():
         x, y = map(int, line.split())
         figuresOnTheBoard.append((x, y))
     file.close()
 
+    # Замер времени начала выполнения программы
     start_time = time.time()
+
+    # Вызов рекурсивной функции для нахождения решений
     recursia(N, L, solutions, figuresOnTheBoard, 0)
+
+    # Замер времени завершения выполнения программы
     end_time = time.time()
 
-
-    print(f"Колличество решений: {len(solutions)}")
+    print(f"Количество решений: {len(solutions)}")
     print(f"Время работы: {end_time - start_time}")
 
+    # Если найдены решения, записываем их в файл
     if solutions:
         uniqueSolutions = {tuple(sorted(solution)) for solution in solutions}
         stringSolution = [" ".join(map(str, solution)) + "\n" for solution in uniqueSolutions]
