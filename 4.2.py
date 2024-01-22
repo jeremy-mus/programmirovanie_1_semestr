ad0a502633638771bbb42f897f2e4ac48e351df5
jeremy-mus <145211896+jeremy-mus@users.noreply.github.com>
2024-01-22 22:14:43 +0300

    Update 4.2.py

--- 4.2.py
+++ 4.2.py
@@ -1,12 +1,20 @@
-from itertools import permutations
-
 def Permutations(n):
-    # Создаем множество уникальных перестановок и преобразуем его в список списков
-    return list(map(list, set(permutations(n))))
+    # Если входной список пуст, возвращаем список, содержащий один пустой список
+    if not n:
+        return [[]]
+    result = []
+
+    for i in range(len(n)):
+        newList = n[i]
+        neitherNums = n[:i] + n[i + 1:]
+
+    # Рекурсивный вызов Permutations для оставшихся элементов
+        for i in Permutations(neitherNums):
+            result.append([newList] + i)
+
+    return result
 
 if __name__ == "__main__":
-    # Выводим результат, запрашивая ввод пользователя в виде строки и преобразуем в список целых чисел
     print("Output:", Permutations(list(map(int, input("Input: ")[1:-1].split(",")))))
-
 
 # Работает верно
